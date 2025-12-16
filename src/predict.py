afd449ad1514d8817a2aff11c3f4653252161948
JuanRaigoso <141664317+JuanRaigoso@users.noreply.github.com>
2025-12-15 19:07:38 -0500

    modelo v2

--- src/predict.py
+++ src/predict.py
@@ -94,3 +94,4 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=None)
     args = parser.parse_args()
     predict_image(args.image, temperature=args.temperature)
+## Final
